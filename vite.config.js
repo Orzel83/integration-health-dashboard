f63f92c1657fc9010e4abdeeb040944544306856
Reszka <marcin.reszka@capgemini.com>
2026-06-19 09:47:05 +0100

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/INTEGRATION-HEALTH-DASHBOARD/',
+  base: '/integration-health-dashboard/',
   plugins: [react()],
 });
